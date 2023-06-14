0bffc28efd151b75260d66b5e49c71104e3593cd
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2023-06-14 14:32:31 +0900

    read / write lock

---
+++ Server/Server/ServerCore/Lock.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    // 재귀적 락 허용 o
+    // 스핀락 5000번 -> yield
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+         
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;
+
+        public void WriteLock()
+        {
+            // 동일 스레드가 writeLock을 이미 획득하고 있는지 확인
+            int lockThreadID = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadID)
+            {
+                _writeCount++;
+                return;
+            }
+
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+                        return;
+                    }
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            int lockCount = --_writeCount;
+            if (lockCount == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        {
+            // 동일 스레드가 writeLock을 이미 획득하고 있는지 확인
+            int lockThreadID = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadID)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
+            // 아무도 wirtelock을 획득하고 있지 않으면 readCount 를 1 늘린다
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; ++i)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;
+                        
+                    // 밑 코드를 원자적으로 하면 위처럼 된다
+                    //if ((_flag & WRITE_MASK) == 0)
+                    //{
+                    //    _flag = _flag + 1;
+                    //    return;
+                    //}
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag);
+        }
+    }
+}
---
+++ Server/Server/ServerCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
